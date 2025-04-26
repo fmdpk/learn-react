7d92d5d1a024cd0163535e90506341c641bea434
farzin mossaed <fmossaed@gmail.com>
2025-04-26 11:20:52 +0330

    work on react

--- src/features/components/Form.jsx
+++ src/features/components/Form.jsx
@@ -1,37 +1,56 @@
 import { useState } from "react";
 export default function Form() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+  const [person, setPerson] = useState({
+    firstName: "",
+    lastName: "",
+  });
 
-  function handleFirstNameChange(e) {
-    setFirstName(e.target.value);
-  }
-
-  function handleLastNameChange(e) {
-    setLastName(e.target.value);
-  }
+  const handleInputChange = (event) => {
+    setPerson((prevValue) => {
+      return {
+        ...prevValue,
+        [event.target.name]: event.target.value,
+      };
+    });
+  };
 
   function handleReset() {
-    setFirstName("");
-    setLastName("");
+    setPerson((prevValue) => {
+      return {
+        ...prevValue,
+        firstName: "",
+        lastName: "",
+      };
+    });
   }
 
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
-      <input
-        placeholder="First name"
-        value={firstName}
-        onChange={handleFirstNameChange}
-      />
-      <input
-        placeholder="Last name"
-        value={lastName}
-        onChange={handleLastNameChange}
-      />
-      <h1>
-        Hi, {firstName} {lastName}
-      </h1>
-      <button onClick={handleReset}>Reset</button>
+    <form
+      onSubmit={(e) => e.preventDefault()}
+      className="flex flex-col items-center justify-center bg-gray-100 gap-4 p-4"
+    >
+      <div className="flex items-center justify-center gap-4">
+        <input
+          placeholder="Enter First name..."
+          name="firstName"
+          value={person.firstName}
+          className="p-3 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none"
+          onChange={handleInputChange}
+        />
+        <input
+          placeholder="Enter Last name..."
+          name="lastName"
+          value={person.lastName}
+          className="p-3 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none"
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1>
+          Hi, {person.firstName} {person.lastName}
+        </h1>
+        <button onClick={handleReset}>Reset</button>
+      </div>
     </form>
   );
 }
